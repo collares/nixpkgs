89d3b8ad05ff6875c508dc0a74124f30194c5355
Mauricio Collares <mauricio@collares.org>
2025-09-07 17:46:20 -0300

    python313Packages.rpy2-rinterface: init at 3.6.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16102,6 +16102,10 @@ self: super: with self; {
 
   rpy2 = callPackage ../development/python-modules/rpy2 { inherit (pkgs) zstd; };
 
+  rpy2-rinterface = callPackage ../development/python-modules/rpy2-rinterface {
+    inherit (pkgs) zstd;
+  };
+
   rpyc = callPackage ../development/python-modules/rpyc { };
 
   rq = callPackage ../development/python-modules/rq { };
